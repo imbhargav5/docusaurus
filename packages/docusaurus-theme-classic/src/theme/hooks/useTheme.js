f708ed849b3f54adfd2c14ab8e7b4f4545040a8e
Yangshun Tay <tay.yang.shun@gmail.com>
2019-12-21 15:50:00 +0800

    feat(v2): useThemeContext

--- packages/docusaurus-theme-classic/src/theme/hooks/useTheme.js
+++ packages/docusaurus-theme-classic/src/theme/hooks/useTheme.js
@@ -4,7 +4,8 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import * as React from 'react';
+
+import React from 'react';
 
 const useTheme = () => {
   const [theme, setTheme] = React.useState(
